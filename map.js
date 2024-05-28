7292acb35657bffcfea56afac793b6181cdcb574
mrjokeller <jonikeller98@gmail.com>
2024-05-28 20:11:11 +0800

    [Feature] Update Country Filter checkboxes to label

--- map.js
+++ map.js
@@ -1,23 +1,34 @@
+
 let map;
 let markerGroup;
 var geojsonData; // Variable to store the GeoJSON data
 
+var selectedCountries = new Set();
+var selectedCategories = new Set();
+
 var countries = new Set();
 
 function initMap() {
+    var mapbox_url = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibXJqb2tlbGxlciIsImEiOiJjbHdweXQ3aWMyb2Q3MmptbTllNnphZHNqIn0.KHaYluQDTamL9J8chbu0wA';
+    var mapbox_attribution = '© Mapbox © OpenStreetMap Contributors';
+    var esri_url = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}';
+    var esri_attribution = '© Esri © OpenStreetMap Contributors';
+    var lyr_satellite = L.tileLayer(esri_url, { id: 'MapID', maxZoom: 20, tileSize: 512, zoomOffset: -1, attribution: esri_attribution });
+    var lyr_streets = L.tileLayer(mapbox_url, { id: 'mapbox/streets-v12', maxZoom: 28, tileSize: 512, zoomOffset: -1, attribution: mapbox_attribution });
+
     map = L.map("map", {
         zoomControl: false,
     }).setView([20, 0], 3);
     markerGroup = L.featureGroup().addTo(map);
-    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    /* L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution:
             'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
         maxZoom: 20,
-    }).addTo(map);
-
+    }).addTo(map); */
+    lyr_streets.addTo(map);
     L.control.zoom({ position: "bottomright" }).addTo(map);
 
-    document.getElementById('searchField').addEventListener('keyup', search);
+
 
     loadDataPoints();
 }
@@ -33,7 +44,7 @@ function loadDataPoints() {
         .then(data => {
             geojsonData = data;
             extractCountries(data);
-            createCountryButton();
+            createCountryFilter();
             updateMarkers();
         })
         .catch(error => {
@@ -48,6 +59,56 @@ function extractCountries(data) {
             countries.add(feature.properties.country);
         }
     });
+}
+
+function createCountryFilter() {
+    var container = document.getElementById("countryButtonContainer");
+    countries.forEach(country => {
+        const fieldset = document.createElement('fieldset');
+        fieldset.classList.add('checkbox-group');
+
+        const checkboxDiv = document.createElement('div');
+        checkboxDiv.classList.add('checkbox');
+
+        const label = document.createElement('label');
+        label.classList.add('checkbox-wrapper');
+
+        const input = document.createElement('input');
+        input.type = 'checkbox';
+        input.classList.add('checkbox-input');
+
+        const checkboxTile = document.createElement('span');
+        checkboxTile.classList.add('checkbox-tile');
+
+        const checkboxLabel = document.createElement('span');
+        checkboxLabel.classList.add('checkbox-label');
+        checkboxLabel.textContent = country;
+
+        const buttonExpand = document.createElement('button');
+        buttonExpand.classList.add('button-expand');
+
+        const svgIcon = document.createElement('i');
+        svgIcon.classList.add('fa-solid', 'fa-expand');
+        // Append SVG icon to button
+        buttonExpand.appendChild(svgIcon);
+
+        // Append label and button to checkbox tile
+        checkboxTile.appendChild(checkboxLabel);
+        checkboxTile.appendChild(buttonExpand);
+
+        // Append input and checkbox tile to label
+        label.appendChild(input);
+        label.appendChild(checkboxTile);
+
+        // Append label to checkbox div
+        checkboxDiv.appendChild(label);
+
+        // Append checkbox div to container
+        fieldset.appendChild(checkboxDiv);
+
+        // Append the container to the body or any other desired parent element
+        container.appendChild(fieldset);
+    })
 }
 
 function createCountryButton() {
@@ -80,7 +141,7 @@ function updateMarkers() {
 
     L.geoJSON(geojsonData, {
         onEachFeature: function (feature, layer) {
-            layer.bindPopup(`${feature.properties.name} - ${feature.properties.country}`).openPopup();
+            layer.bindPopup(`<b>${feature.properties.name} - ${feature.properties.country}</b>`).openPopup();
         }
     }).addTo(markerGroup);
 }
